eab7005040e9c89830452797ac3d2626f82e90d5
Andy Nguyen <badandytowin@gmail.com>
2020-09-13 22:19:41 -0500

    add unit test packages

---
+++ HandlebarsParserTests/Tests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace MyFirstUnitTests
+{
+    public class Tests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(3, Add(1, 2));
+        }
+
+        int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
